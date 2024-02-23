b8f78d1adb0e60d18560269adef563d7cba8ee2d
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-23 20:41:50 +0530

    fix: row names cell added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let rows = 100
 let cols = 26
 
-// let addressColCont = document.querySelector("")
+let rowContainer = document.querySelector(".row-names-container")
+
+for(i=1; i<=rows; i++){
+    let rowNames = document.createElement("div")
+    rowNames.innerText = i
+    rowNames.classList = "row-name"
+    rowContainer.appendChild(rowNames)
+    console.log("ooooooo")
+}
